e4a1e93c78c4145579ee9591616ae029172994f8
Tobi Wydra <163324193+tobiaswydra17@users.noreply.github.com>
2024-06-05 15:51:18 +0200

    Update navbar.php
    neue navbar

--- ToDoList-main/nav/navbar.php
+++ ToDoList-main/nav/navbar.php
@@ -1,6 +1,13 @@
-<nav>
-    <a id="Startseite" href="../pages/welcome.php">Startseite</a>
-    <a id="ToDoList" href="../pages/index.php">Meine Tasks</a>
-    <a id="Login" href="../pages/login.php">Login</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="../pages/welcome.php">To-Do List</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#calendarModal">Calendar</a>
+            </li>
+        </ul>
+    </div>
 </nav>
-
